c9fb05f5704886d2537a0acecc6884289420c346
Greg Hale <imalsogreg@gmail.com>
2019-08-02 22:08:14 -0400

    Use CLI filename in xcrust-cat-pos

--- src/bin/xcrust-cat-pos.rs
+++ src/bin/xcrust-cat-pos.rs
@@ -1,10 +1,12 @@
+use std::env;
 use std::path::{Path};
 
 use xcrust::pos::mwl_ad::{read_p};
 
 fn main() {
-    let file = "../mwsoft64/tests/data/original.p";
-    let ps = read_p( Path::new(file) );
+    let args : Vec<String> = env::args().collect();
+    let file = &args[1];
+    let ps = read_p( Path::new(&file) );
     println!("ps[0]: {:?}", ps[0]);
     println!("ps[0]: {:?}", ps[0]);
 }
